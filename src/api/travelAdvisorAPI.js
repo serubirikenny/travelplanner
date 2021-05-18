2d39beba1d66babdf693f36c5aebc3751cfb4da0
serubirikenny <Kserubiri@gmail.com>
2021-05-18 12:00:00 +0300

    implementation of weather data method

--- src/api/travelAdvisorAPI.js
+++ src/api/travelAdvisorAPI.js
@@ -23,5 +23,19 @@ export const getPlacesData = async (type, sw, ne) => {
 };
 
 export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
 
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
