f3c5de93978b179659cb440836bf378d81f87600
serubirikenny <Kserubiri@gmail.com>
2021-05-28 12:00:00 +0300

    added imports

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles.js';
 
 const Header = () => {
   
