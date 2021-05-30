38adb085543df925bb9d8e94fefd1926e2bd246e
serubirikenny <Kserubiri@gmail.com>
2021-05-30 12:00:00 +0300

    added Header component elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,29 @@ import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './styles.js';
 
 const Header = ({ onPlaceChanged, onLoad }) => {
-  
+  const classes = useStyles();
 
   return (
-  <h1>Hello World</h1>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
