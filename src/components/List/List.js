41fdaa230c07004144764fd2fce86991ab80f466
serubirikenny <Kserubiri@gmail.com>
2021-06-09 12:00:00 +0300

    added props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,7 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 import useStyles from './styles.js';
 
 
-const List = () => {
+const List = ({ places, type, setType, rating, setRating, childClicked, isLoading }) => {
  
   return (
     <h1>List</h1>
