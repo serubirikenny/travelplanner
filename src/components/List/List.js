a4f71d8036dd4b7f720f0123d11e99c10a310ccd
serubirikenny <Kserubiri@gmail.com>
2021-06-09 12:00:00 +0300

    added imports

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
 
 const List = () => {
